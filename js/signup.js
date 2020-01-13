c9d5df99c15609e9a15ca402e877c987919135a9
Scott Sauceda <msauceda0812@gmail.com>
2020-01-13 13:11:50 -0500

    added signup.js

---
+++ js/signup.js
@@ -0,0 +1,57 @@
+let signupForm = document.querySelector(".form-signup");
+
+// If it isn't "undefined" and it isn't "null", then it exists.
+if(typeof(loginForm) != "undefined" && loginForm != null){
+    loginForm.addEventListener("submit", loginUser);
+    console.log("login page");
+} else if(typeof(signupForm) != "undefined" && signupForm != null) {
+    document.querySelector(".form-signup").addEventListener("submit", signupUser);
+    console.log("signup page");
+} else {
+    // console.log("none");
+}
+
+// function for user sign up
+function signupUser(event){
+    console.log("signupUser called");
+    event.preventDefault();
+    //let inputEmail = document.getElementById("inputEmail").value;
+    //console.log(inputEmail);
+    let inputUsername = document.getElementById("inputUsername").value;
+    console.log(inputUsername);
+    let inputPassword = document.getElementById("inputPassword").value;
+    console.log(inputPassword);
+
+    fetch("http://localhost:8080/redditBackedn/user/signup", {
+        method: "post",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            //email: inputEmail,
+            username: inputUsername,
+            password: inputPassword
+        })
+    })
+    .then((response) => {
+        return response.json();
+    })
+    .then(function(json){
+        console.log(json);
+        if(json.httpStatus != "BAD_REQUEST"){
+            alert("New User Created");
+            sessionStorage.setItem("userToken", json.token);
+            sessionStorage.setItem("userLoginStatus", true);
+            sessionStorage.setItem("username", inputUsername);
+            window.location.replace("index.html");
+        } else {
+            alert("Failed To Create User");
+            location.reload();
+        }
+    })
+    .catch(function(error){
+        console.log(error);
+        alert("Failed To Create User");
+    })
+}
