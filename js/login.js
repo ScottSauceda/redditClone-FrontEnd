99babe1c56340c0cc49bf5f8dbf922cff65c8b4e
Scott Sauceda <msauceda0812@gmail.com>
2020-01-13 12:32:06 -0500

    added login.js code

--- js/login.js
+++ js/login.js
@@ -1,1 +1,57 @@
-console.log("Hello World form logn.js!");
+console.log("Hello World from login.js");
+
+let loginForm = document.querySelector(".form-login");
+let signupForm = document.querySelector(".form-signup")
+
+// If it isn't "undefined" and it isn't "null", then it exists.
+if(typeof(loginForm) != "undefined" && loginForm != null){
+ loginForm.addEventListener("submit", loginUser);
+ console.log("login page");   
+} else if(typeof(signupForm) != "undefined" && signupForm != null) {
+    document.querySelector(".form-signup").addEventListener("submit", signupUser);
+    console.log("signup page");
+} else {
+    // console.log("none");
+}
+
+// login function
+function loginUser(event){
+    event.preventDefault();
+    let logEmail = document.getElementById("inputEmail").value;
+    let logPassword = document.getElementById("inputPassword").value;
+
+    fetch("http://localhost:8080/redditBackend/user/login", {
+        method: "post",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type" : "application/json"
+        },
+        body: JSON.stringify({
+            username: logEmail,
+            password: logPassword
+        })
+    })
+    .then((response) => {
+        return response.json();
+    })
+    .then(function(json){
+        console.log("REACHED???")
+        console.log(">>>>>>" + json);
+
+        // if(json.httpStatus != "BAD_REQUEST"){
+        if(json.token != null){
+            console.log(json.token);
+            sessionStorage.setItem("userToken", json.token);
+            sessionStorage.setItem("userLoginStatus", true);
+            sessionStorage.setItem("username", logEmail); // json object won't return a username since its just a token, updated it to set username to user defined value
+            window.location.replace("index.html");
+        } else {
+            alert("incorrect credentials");
+            location.reload(); //error msg
+        }
+    })
+    .catch(function(error){
+        console.log(".catch")
+        console.log(error);
+    })
+}
